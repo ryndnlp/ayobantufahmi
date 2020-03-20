440d676c5e38af2b0aca5165543844e264876def
thoriqa <thoriqakbar26@gmail.com>
2020-03-20 10:32:37 +0700

    Update button.java

--- StandardCalculator/src/button.java
+++ StandardCalculator/src/button.java
@@ -14,61 +14,61 @@ abstract class operandButton extends javax.swing.JButton{
 
 class oneButton extends numberButton{
     public void number(){
-        stack.push(1);
+        stack.push("1");
     }
 }
 
 class twoButton extends numberButton{
     public void number(){
-        stack.push(2);
+        stack.push("2");
     }
 }
 
 class threeButton extends numberButton{
     public void number(){
-        stack.push(3);
+        stack.push("3");
     }
 }
 
 class fourButton extends numberButton{
     public void number(){
-        stack.push(4);
+        stack.push("4");
     }
 }
 
 class fiveButton extends numberButton{
     public void number(){
-        stack.push(5);
+        stack.push("5");
     }
 }
 
 class sixButton extends numberButton{
     public void number(){
-        stack.push(6);
+        stack.push("6");
     }
 }
 
 class sevenButton extends numberButton{
     public void number(){
-        stack.push(7);
+        stack.push("7");
     }
 }
 
 class eightButton extends numberButton{
     public void number(){
-        stack.push(8);
+        stack.push("8");
     }
 }
 
 class nineButton extends numberButton{
     public void number(){
-        stack.push(9);
+        stack.push("9");
     }
 }
 
 class zeroButton extends numberButton{
     public void number(){
-        stack.push(0);
+        stack.push("0");
     }
 }
 
@@ -86,49 +86,73 @@ class minusButton extends operandButton{
 
 class timesButton extends operandButton{
     public void operand(){
-        stack.push("x");
+        stack.push("*");
     }
 }
 
-class plusButton extends operandButton{
+class divisionButton extends operandButton{
     public void operand(){  
-        stack.push("+");
+        stack.push("/");
+    }
+}
+
+class powerToButton extends operandButton{
+    public void operand(){  
+        stack.push("^");
+    }
+}
+
+class sinButton extends operandButton{
+    public void operand(){  
+        stack.push("sin");
     }
 }
 
 class cosButton extends operandButton{
     public void operand(){  
-        stack.push("+");
-    }
-}
-
-class sineButton extends operandButton{
-    public void operand(){  
-        stack.push("+");
+        stack.push("cos");
     }
 }
 
 class tanButton extends operandButton{
     public void operand(){  
-        stack.push("+");
+        stack.push("tan");
+    }
+}
+class ACosButton extends operandButton{
+    public void operand(){  
+        stack.push("acos");
+    }
+}
+
+class ASinButton extends operandButton{
+    public void operand(){  
+        stack.push("asin");
+    }
+}
+
+class ATanButton extends operandButton{
+    public void operand(){  
+        stack.push("tan");
     }
 }
 
 class logButton extends operandButton{
     public void operand(){  
-        stack.push();
+        stack.push("log");
     }
 }
 class rootButton extends operandButton{
     public void operand(){  
-        stack.push();
+        stack.push("sqrt");
     }
 }
 
-
-
-
-
+class ansButton extends operandButton{
+    public void store(string form){
+        form.solve();
+    }
+}
 
 class mrButton extends memoryButton{
     public void store(){
@@ -143,11 +167,7 @@ class mcButton extends memoryButton{
     }
 }
 
-class ansButton extends memoryButton{
-    public void store(){
-        solve();
-    }
-}
+
 
 
 
