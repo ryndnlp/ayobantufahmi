f94df622a7d5c95aad20d9d3ed59a28a361e67f8
ryndnlp <ryandaniel216@gmail.com>
2020-03-21 15:14:30 +0700

    Minor Fix Button.java

--- StandardCalculator/src/button.java
+++ StandardCalculator/src/button.java
@@ -3,11 +3,9 @@ import javax.swing.JLabel;
 import java.awt.event.ActionEvent;
 
 abstract class Button extends JButton {
-    String text;
 
     public Button(String text) {
         super(text);
-        this.text = text;
     }
     abstract public void onClick(ActionEvent evt, JLabel Layar);
 }
