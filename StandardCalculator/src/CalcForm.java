47c971428d827f0d500d90272d2df0b527c259d9
ryndnlp <ryandaniel216@gmail.com>
2020-03-22 20:38:44 +0700

    Minor Fixes

--- StandardCalculator/src/CalcForm.java
+++ StandardCalculator/src/CalcForm.java
@@ -1,23 +1,14 @@
 import data.expression.*;
 
 import java.awt.event.*;
-import javax.swing.*;
 import java.awt.*;
 
+import javax.swing.*;
 import java.util.*; 
 import java.util.logging.*;
 
 import data.exception.*;
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- *
- * @author ASUS
- */
 public class CalcForm extends JFrame {
 
     /**
