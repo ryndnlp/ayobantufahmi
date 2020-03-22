29ffd966ce5e1802a15285ebe9400b3099c6ae65
ryndnlp <ryandaniel216@gmail.com>
2020-03-22 23:46:54 +0700

    Minor fix kelupaan

--- StandardCalculator/src/gui/CalcForm.java
+++ StandardCalculator/src/gui/CalcForm.java
@@ -345,9 +345,7 @@ public class CalcForm extends JFrame {
                     outExp = ed.parse(inString);
                     if (outExp.solve() - (int)Math.round(outExp.solve()) == 0){
                         T.addToToken(Integer.toString((int)Math.round(outExp.solve())));
-                    }else if(outExp.solve() == null){
-                        throw new InvalidOperandException();
-                    }else{
+                    else{
                         T.addToToken(Double.toString(outExp.solve()));
                     }
                     token.setToken(T);
