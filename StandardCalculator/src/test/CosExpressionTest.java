708697511df4a6af87933c42381d71d418ee10e9
gimhafnu <13518127@std.stei.itb.ac.id>
2020-03-22 21:13:05 +0700

    More expression tests; log expression is now log10 instead of ln

---
+++ StandardCalculator/src/test/CosExpressionTest.java
@@ -0,0 +1,28 @@
+package test;
+
+import java.util.Scanner;
+import data.expression.*;
+import data.expression.unaryExpressions.*;
+import data.expression.binaryExpressions.*;
+import data.exception.*;
+
+public class CosExpressionTest{
+    public static void main(final String args[]) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("x: ");
+        Double x = in.nextDouble();
+        TerminalExpression xt = new TerminalExpression(x);
+        CosExpression ae = new CosExpression(xt);
+        try{        
+            System.out.println(ae + " = "+ae.solve());
+        }catch(InvalidOperandException err){
+            String error = err.PrintMessage();
+        }catch(NotANumberException err){
+            String error = err.PrintMessage();
+        }catch (Exception err) {
+            // TODO Auto-generated catch block
+            // err.printStackTrace();
+        }
+    }
+}
---
+++ StandardCalculator/src/test/PowerToExpressionTest.java
@@ -0,0 +1,31 @@
+package test;
+
+import java.util.Scanner;
+import data.expression.*;
+import data.expression.unaryExpressions.*;
+import data.expression.binaryExpressions.*;
+import data.exception.*;
+
+public class PowerToExpressionTest{
+    public static void main(final String args[]) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("x: ");
+        Double x = in.nextDouble();
+        System.out.print("y: ");
+        Double y = in.nextDouble();
+        TerminalExpression xt = new TerminalExpression(x);
+        TerminalExpression yt = new TerminalExpression(y);
+        PowerToExpression ae = new PowerToExpression(xt,yt);
+        try{        
+            System.out.println("x^y = " + ae + " = "+ae.solve());
+        }catch(InvalidOperandException err){
+            String error = err.PrintMessage();
+        }catch(NotANumberException err){
+            String error = err.PrintMessage();
+        }catch (Exception err) {
+            // TODO Auto-generated catch block
+            // err.printStackTrace();
+        }
+    }
+}
